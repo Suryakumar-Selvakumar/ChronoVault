17dc4ee78533e082b2b316b60967a23ddefd4aec
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2025-02-19 16:13:18 -0700

    feat: Setup server and basic views for homepage

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+
+// setup path for static files
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+// setup view path and engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+});
+
+app.post("/new", (req, res) => {});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
