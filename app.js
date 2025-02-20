bf6ae44ead4ba0a3021ad4be974d073e21329b25
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2025-02-19 17:07:53 -0700

    feat: Add POST logic to add new messages to board

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// setup express middleware to use form's POST data
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -8,6 +8,12 @@ newRouter.get("/", (req, res) => {
   res.render("form");
 });
 
-newRouter.post("/");
+newRouter.post("/", (req, res) => {
+  const { user, text } = req.body;
+
+  messages.push({ text: text, user: user, added: new Date() });
+
+  res.redirect("/");
+});
 
 module.exports = newRouter;
