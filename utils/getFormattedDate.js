0666aadcf818f610ee80bbf8fe1a07de0121ab61
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2025-03-04 21:27:02 -0700

    style: + `view all messages` btn & style it

--- utils/getFormattedDate.js
+++ utils/getFormattedDate.js
@@ -1,14 +1,19 @@
-const { differenceInHours, format } = require("date-fns");
+const { differenceInHours, differenceInMinutes, format } = require("date-fns");
 
 const getFormattedDate = (date) => {
   date = new Date(date);
   const currentDate = new Date();
   const hoursDif = differenceInHours(currentDate, date);
+  const minutesDif = differenceInMinutes(currentDate, date);
 
   const formattedDate = format(date, "MM-dd-yyyy");
 
   if (hoursDif <= 23) {
-    return hoursDif <= 1 ? `${hoursDif} hour ago` : `${hoursDif} hours ago`;
+    return hoursDif < 1
+      ? `${minutesDif} minutes ago`
+      : hoursDif == 1
+      ? `${hoursDif} hour ago`
+      : `${hoursDif} hours ago`;
   } else {
     return `${formattedDate}`;
   }
